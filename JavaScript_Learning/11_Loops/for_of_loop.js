f1347b442abd6a55f1f496d385b07873a73647af
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-27 00:58:10 +0300

    Add for_of_loop solution

---
+++ JavaScript_Learning/11_Loops/for_of_loop.js
@@ -0,0 +1,6 @@
+let name = "JavaScript";
+
+for (let char of name) {
+    console.log("Character: " + char); // Iterates over all characters in "JavaScript" and prints each one on a new line, preceded by "Character: "
+}
+
