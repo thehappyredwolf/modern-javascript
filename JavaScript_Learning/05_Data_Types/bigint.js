2f155f13dccc255fdefaa9ee5a054671d4bf35c0
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 18:31:17 +0300

    Add bigint solution

---
+++ JavaScript_Learning/05_Data_Types/bigint.js
@@ -0,0 +1,3 @@
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log(bigNumber); // Outputs: 1234567890123456789012345678901234567890n
+console.log(typeof bigNumber); // Outputs: bigint
