4978510b091f89f933207cc4a0be4e71cffbec22
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 22:44:47 +0300

    Add do_while_loop solution

---
+++ JavaScript_Learning/11_Loops/do_while_loop.js
@@ -0,0 +1,8 @@
+// The do...while loop is similar to the while loop, but it executes the code block at least once before checking the condition.
+
+let count = 5;
+
+do {
+    console.log("Count is: " + count); // This prints once.
+    count++;
+} while (count < 5); // The condition is false, so the loop exits immediately after printing the message one time.
