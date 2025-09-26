cb2057ad2d186dd08cfd1fbd6c80edb26d96a681
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 07:37:24 +0300

    Add statements solution

---
+++ JavaScript_Learning/02_Code_Structure/statements.js
@@ -0,0 +1,16 @@
+let message = "Hello";
+console.log(message);
+message = "World";
+console.log(message);
+
+// Still printing boring messages to the console, but I noticed that each statement shows up on a new line and ends with a semicolon.
+
+
+// This is a single-line comment
+
+/*
+This is a
+multi-line comment
+*/
+
+console.log("Comments don't affect the code execution"); // Yep, that's true. Comments are meant for humans, not machines.
