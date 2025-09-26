b82d233778d925454fa371fb5afee6f464df7663
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 18:35:43 +0300

    Add coercion solution

---
+++ JavaScript_Learning/05_Data_Types/coercion.js
@@ -0,0 +1,6 @@
+// JavaScript is a loosely typed language, which means it allows implicit type coercion when operations involve mismatched types.
+
+console.log(5 + "5"); // Outputs: 55 (string)
+console.log("5" - 3); // Outputs: 2 (number)
+console.log("5" * "3"); // Outputs: 15 (number)
+console.log(true + 1); // Outputs: 2 (number)
