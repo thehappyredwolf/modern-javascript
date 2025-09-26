9bb81a55a095b543f6247f03b05407e15011c52f
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 17:36:08 +0300

    Add examples of using const to declare variables

--- JavaScript_Learning/04_Variables/variables.js
+++ JavaScript_Learning/04_Variables/variables.js
@@ -11,3 +11,16 @@ console.log(name); // Outputs: Alice
 
 name = "Bob"; // We can also change string values.
 console.log(name); // Outputs: Bob
+
+// **************************************************************************
+
+const PI = 3.14159;
+console.log(PI); // Outputs: 3.14159
+
+// PI = 3.14; // This would cause an error: Assignment to a constant variable.
+
+const person = {name: "Charlie", age: 30};
+person.age = 31; // We can modify properties of a const object.
+console.log(person); // Outputs: {name: "Charlie", age: 31}
+
+// person = {name: "David", age: 25}; // This would cause an error: Assignment to a constant variable.
