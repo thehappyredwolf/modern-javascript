5d5c6dc143354b9f129dc57808fa26298be077be
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 16:51:46 +0300

    Add strict solution

---
+++ JavaScript_Learning/03_Use_Strict/strict.js
@@ -0,0 +1,5 @@
+'use strict';
+
+x = 3.14;  // This will cause an error because we didn’t use a keyword (let, const, or var) to declare the variable.
+// Strict mode requires all variables to be declared before use.
+console.log(x);
