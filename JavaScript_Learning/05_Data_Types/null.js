52e68f856fd9d16d6a75440bc6b17728fc361e35
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 18:30:08 +0300

    Add null solution

---
+++ JavaScript_Learning/05_Data_Types/null.js
@@ -0,0 +1,3 @@
+let nullVariable = null;
+console.log(nullVariable); // Outputs: null
+console.log(typeof nullVariable); // Outputs: object (this is a well known bug in JavaScript, typeof operator is not working correctly there)
