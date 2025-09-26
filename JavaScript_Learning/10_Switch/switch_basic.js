5b2c25887d9209f050b14639cf0b1fb1c0a41cfb
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 22:20:59 +0300

    Add switch_basic solution

---
+++ JavaScript_Learning/10_Switch/switch_basic.js
@@ -0,0 +1,30 @@
+let day = 3;
+let dayName;
+
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day";
+}
+
+console.log(dayName); // Outputs: Wednesday
