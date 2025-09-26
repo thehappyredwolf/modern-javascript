d5a7352ea20bb91b1b9205cf311ff1d3faf9d3fc
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 07:21:21 +0300

    Add an additional message

--- JavaScript_Learning/01_Hello_World/hello_world.js
+++ JavaScript_Learning/01_Hello_World/hello_world.js
@@ -1,1 +1,6 @@
 console.log("Hello, World!") // Obviously, the string "Hello, World!" will be printed to the console.
+
+// Add another console.log() statement to print an additional message, like your name or today's date.
+// Well, I decided to print a famous quote from a relatively old movie.
+
+console.log("Abdo Mouta rege3 ya 7ara!"); // From the movie "Abdo Mouta" (released in 2012).
