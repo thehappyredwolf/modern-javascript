2330fd15e50421d04131c3484bd9d77f4a5c68b3
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 21:15:21 +0300

    Add concat method examples

--- JavaScript_Learning/08_String_Concatenation/traditional_concat.js
+++ JavaScript_Learning/08_String_Concatenation/traditional_concat.js
@@ -14,3 +14,19 @@ console.log(introduction);  // Outputs: My name is John Doe and I am 30 years ol
 let message = "Hello";
 message += " World";
 console.log(message);  // Outputs: Hello World
+
+
+// *******************************************************************************
+
+let str1 = "Hello";
+let str2 = "World";
+let str3 = "!";
+
+let result = str1.concat(" ", str2, str3); // same as str1 + " " + str2 + str3
+console.log(result);  // Outputs: Hello World!
+
+// concat() can take multiple arguments
+let greeting = "".concat("Hi", " ", "there", "!");
+console.log(greeting);  // Outputs: Hi there!
+
+// While concat() does the same thing as the '+' operator, it is generally used less often.
