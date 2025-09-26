b54b91fd49b1dc2d8e8d9c31862b8b15fb7b7c4d
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 19:42:47 +0300

    Add examples of implicit conversion

---
+++ JavaScript_Learning/06_Type_Conversions/implicit_conversion.js
@@ -0,0 +1,12 @@
+// String concatenation
+console.log("3" + 4);        // "34"
+console.log(4 + "3");        // "43"
+console.log("3" + true);     // "3true"
+
+// Numeric conversion
+console.log("6" - 2);        // 4
+console.log("6" * "2");      // 12
+
+// Equality operators
+console.log("5" == 5);       // true 
+console.log("5" === 5);      // false
