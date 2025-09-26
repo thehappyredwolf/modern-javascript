0bb40e0ffcd731b46949c3247992f1557fd0eead
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 22:41:23 +0300

    Add for_loop solution

---
+++ JavaScript_Learning/11_Loops/for_loop.js
@@ -0,0 +1,6 @@
+// For Loops
+// This loop prints numbers from 0 to 4, each preceded by the message "Iteration number: "
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
