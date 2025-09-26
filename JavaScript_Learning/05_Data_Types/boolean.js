1fab3ff4d0bcffec7e8274740a8d91a29fcd54a5
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 18:27:20 +0300

    Add boolean solution

---
+++ JavaScript_Learning/05_Data_Types/boolean.js
@@ -0,0 +1,7 @@
+let isGreater = 4 > 1;
+console.log(isGreater); // Outputs: true
+
+let isLess = 4 < 1;
+console.log(isLess);    // Outputs: false
+
+console.log(typeof isGreater); // Outputs: boolean
