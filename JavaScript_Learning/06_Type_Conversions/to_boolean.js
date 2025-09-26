20a2c46e869bdfe6685e65c892822de8f8a3841a
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 19:29:16 +0300

    Add the explicit way to convert to booleans

---
+++ JavaScript_Learning/06_Type_Conversions/to_boolean.js
@@ -0,0 +1,7 @@
+// Using Boolean() function
+console.log(Boolean(1));       // true
+console.log(Boolean(0));       // false
+console.log(Boolean("hello")); // true
+console.log(Boolean(""));      // false
+console.log(Boolean(null));    // false
+console.log(Boolean(undefined));// false
