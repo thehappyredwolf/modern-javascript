adbca4821b08bac2c540d89cc517471ceceb6e04
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-09-26 17:05:35 +0300

    Add examples of declaring variables using let

---
+++ JavaScript_Learning/04_Variables/variables.js
@@ -0,0 +1,13 @@
+'use strict'; // It’s good practice to add this line at the top of all programs.
+
+let age = 25;
+console.log(age); // Outputs: 25
+
+age = 26; // We can change the value.
+console.log(age); // Outputs: 26
+
+let name = "Alice";
+console.log(name); // Outputs: Alice
+
+name = "Bob"; // We can also change string values.
+console.log(name); // Outputs: Bob
